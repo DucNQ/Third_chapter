a9ea941c59ccae06340e649ea6da0f008efb9e56
DucNQ <nguyen.quang.duc@framgia.com>
2014-01-10 14:48:02 +0700

    Add user micropposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy
 	validates :name, 	presence: true, 	length: { maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates 	:email,	
@@ -11,6 +12,10 @@ class User < ActiveRecord::Base
 	has_secure_password
 	before_save { email.downcase! }
 	before_create :create_remember_token
+
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
